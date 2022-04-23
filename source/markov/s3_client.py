ffa0b33f135879f98c394eaab2e60ed3f05dea5c
Jack Sullivan <44463687+jx34tech@users.noreply.github.com>
2022-04-24 07:49:12 +1000

    Added additional logging logic

--- source/markov/s3_client.py
+++ source/markov/s3_client.py
@@ -162,6 +162,9 @@ class SageS3Client():
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 logger.info("Exception [{}] occured on download file-{} from s3 bucket-{} key-{}".format(e.response['Error'], local_path, self.bucket, s3_key))
+                logger.info(local_path.format(e.response['Error']))
+                logger.info(self.bucket.format(e.response['Error']))
+                logger.info(s3_key.format(e.response['Error']))
                 return False
             else:
                 utils.json_format_logger("boto client exception error [{}] occured on download file-{} from s3 bucket-{} key-{}"
